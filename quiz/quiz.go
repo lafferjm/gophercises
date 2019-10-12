07aea9f5a036de4b76c26b7f2165dfdd945cbc33
Justin Lafferty <lafferjm@gmail.com>
2019-10-12 15:39:28 -0500

    watched video decided to add the problem struct

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -27,6 +27,24 @@ func getQuestions(problemsFile string) ([][]string, error) {
 	return records, nil
 }
 
+type problem struct {
+	question string
+	answer   string
+}
+
+func parseProblems(problems [][]string) []problem {
+	ret := make([]problem, len(problems))
+
+	for i, questionAnswer := range problems {
+		ret[i] = problem{
+			question: questionAnswer[0],
+			answer:   questionAnswer[1],
+		}
+	}
+
+	return ret
+}
+
 func main() {
 	problemsFile := flag.String("problems", "problems.csv", "a csv file with the quize problems")
 	timerLength := flag.Int("timer", 30, "amount of time to take the quiz in seconds")
@@ -42,14 +60,16 @@ func main() {
 		fmt.Println("Error encountered while getting the questions!")
 	}
 
+	problems := parseProblems(records)
+
 	questionsCorrect := 0
 
 	inputReader := bufio.NewReader(os.Stdin)
 	answersChannel := make(chan string)
 
 questionsLoop:
-	for _, question := range records {
-		fmt.Printf("What is %s? ", question[0])
+	for _, question := range problems {
+		fmt.Printf("What is %s? ", question.question)
 		go func() {
 			answer, _ := inputReader.ReadString('\n')
 			answersChannel <- answer
@@ -60,11 +80,11 @@ questionsLoop:
 			fmt.Println()
 			break questionsLoop
 		case answer := <-answersChannel:
-			if strings.TrimRight(answer, "\n") == question[1] {
+			if strings.TrimRight(answer, "\n") == question.answer {
 				questionsCorrect++
 			}
 		}
 	}
 
-	fmt.Printf("You have completed the quiz.  Results %d/%d correct, way to go!\n", questionsCorrect, len(records))
+	fmt.Printf("You have completed the quiz.  Results %d/%d correct, way to go!\n", questionsCorrect, len(problems))
 }
